4499597be0fd85128d26556fa6ce139d872013ef
ddozza54 <100435210+ddozza54@users.noreply.github.com>
2023-02-20 12:48:21 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('/dev/stdin').toString();
+const score = Number(input);
+
+if (90 <= score && score <= 100)
+    console.log("A");
+else if (80 <= score && score < 90
+) console.log("B");
+else if (70 <= score && score < 80
+) console.log("C");
+else if (60 <= score && score < 70
+) console.log("D");
+else console.log("F");
