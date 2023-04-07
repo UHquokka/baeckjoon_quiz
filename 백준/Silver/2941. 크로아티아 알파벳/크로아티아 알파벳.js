7e3c6c6e319b3fdd6f858f34b1f76380c8c7c439
ddozza54 <bananamayong@gmail.com>
2023-04-07 12:06:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -3,6 +3,6 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 for (let i = 0; i < cro.length; i++) {
-    input = input.split(cro[i]).join("C");
+    input = input.replaceAll(cro[i], "C");
 }
 console.log(input.length);
