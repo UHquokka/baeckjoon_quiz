436639d8693285350b887ae23681bce2c6627458
ddozza54 <bananamayong@gmail.com>
2023-04-27 10:44:00 +0900

    [level 0] Title: 등수 매기기, Time: 2.55 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,14 @@
+function solution(score) {
+    let avr = [];
+    for (let i = 0; i < score.length; i++) {
+        avr.push(score[i].reduce((sum, cur) => sum + cur) / 2)
+    }
+    const sortArr = avr.map(v => v).sort((a, b) => b - a);
+    let index = [];
+    for (let i = 0; i < avr.length; i++) {
+        console.log(avr[i])
+        index.push((sortArr.findIndex(v => v == avr[i]) + 1)
+        )
+    }
+    return index;
+}
