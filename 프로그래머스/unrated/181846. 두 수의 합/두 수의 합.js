41899d63ca511e76a3e0b59ff72c9d8abac0b3cd
ddozza54 <bananamayong@gmail.com>
2023-06-27 08:42:08 +0900

    [unrated] Title: 두 수의 합, Time: 38.28 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-    const bigInt = Number.MAX_SAFE_INTEGER;
-    if (a <= bigInt && b <= bigInt) {
-        return Number(a) + Number(b) + '';
-    } else if (a > bigInt || b > bigInt) {
- return BigInt(a) + BigInt(b)+'';
-    }
+        return BigInt(a) + BigInt(b)+'';
 }
