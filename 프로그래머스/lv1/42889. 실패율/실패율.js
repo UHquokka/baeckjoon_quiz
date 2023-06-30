84016dc680d59d88bfd820630d1ab7a28cca7679
ddozza54 <bananamayong@gmail.com>
2023-06-30 15:40:06 +0900

    [level 1] Title: 실패율, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,15 +1,12 @@
 function solution(N, stages) {
     let challengeCount = stages.length;
-    let successCount = 0;
-    let failCount = 0;
     let failRate = [];
-
+    let failCount = 0;
     for (let i = 1; i <= N; i++) {
+        let successCount = stages.filter(v => v > i).length
         challengeCount -= failCount;
-        successCount = stages.filter(v => v > i).length
         failCount = challengeCount - successCount;
-
         failRate.push([i, failCount / challengeCount])
     }
-    return failRate.sort((a, b) => b[1] - a[1]).map(v => Number(v[0]))
+    return failRate.sort((a, b) => b[1] - a[1]).map(v => (v[0]))
 }
