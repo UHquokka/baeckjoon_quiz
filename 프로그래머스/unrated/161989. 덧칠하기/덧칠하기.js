89297ecbbbeb9c05faebfd73116a220591913896
ddozza54 <bananamayong@gmail.com>
2023-07-04 15:46:18 +0900

    [unrated] Title: 덧칠하기, Time: 8.37 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,16 @@
+function solution(n, m, sections) {
+    const line = Math.floor(n / m);
+    let allSection = new Array(n + 1).fill(0);
+    Array.from(sections, x => allSection[x] = -1);
+
+    let count = 0;
+    sections.map(s => {
+        if (allSection[s] === -1) {
+            count++;
+            for (let i = s; i < s + m; i++) {
+                allSection[i] += 1;
+            };
+        }
+    })
+    return count;
+}
