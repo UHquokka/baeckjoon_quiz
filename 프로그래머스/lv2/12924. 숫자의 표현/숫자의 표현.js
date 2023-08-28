e1f37238563732356253bd62365cf4f06d800c01
ddozza54 <bananamayong@gmail.com>
2023-08-28 16:22:37 +0900

    [level 2] Title: 숫자의 표현, Time: 1.75 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let count = 1;
+    if (n == 1) {return 1}
+    else{
+    for (let i = 1; i <= Math.ceil(n / 2); i++) {
+        let sum = 0;
+        let start = i;
+        while (sum < n) {
+            sum += start;
+            if (sum === n) count++;
+            start++;
+        }
+    }
+    return count;
+}}
