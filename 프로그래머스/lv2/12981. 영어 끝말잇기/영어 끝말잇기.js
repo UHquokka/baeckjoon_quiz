572b50e0ea2a9de4c5d13bdafa72b295fe15fe4a
ddozza54 <bananamayong@gmail.com>
2023-09-07 10:33:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,24 +1,10 @@
 function solution(n, words) {
-    const result = [0, 0];
-    let breakNumber = 0;
     for (let i = 0; i < words.length; i++) {
-        if (words[i].length < 2) {
-            break;
-        }
         if (i > 0) {
-            if ([...words[i - 1]].pop() != words[i][0]) {
-                breakNumber = i;
-                break;
-            }
-            if ([...words].splice(0, i - 1).includes(words[i])) {
-                breakNumber = i;
-                break;
+            if (i > 0 && [...words[i - 1]].pop() != words[i][0] || [...words].splice(0, i - 1).includes(words[i])) {
+                return [i % n + 1, Math.ceil((i + 1) / n)]
             }
         }
     }
-    if (breakNumber !== 0) {
-        result[0] = breakNumber % n + 1;
-        result[1] = Math.ceil((breakNumber + 1) / n);
-    }
-    return result
+    return [0, 0]
 }
