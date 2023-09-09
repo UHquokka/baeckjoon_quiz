7be5f128e715e84d038e3a95304724cb0143f641
ddozza54 <bananamayong@gmail.com>
2023-09-09 09:54:47 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2631.67 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,12 +2,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    //개 만큼의 *을 만든다.
-    //만든 a 를 b 만큼 출력한다.
-    let letterA = '';
-    for(i=0; i<a; i++){
-        letterA += '*' 
-    }
+
+    let letterA = '*'.repeat(a)
     for(i=0; i<b; i++){
 console.log(letterA)
     }
