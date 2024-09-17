992d699c26df39268e262e7795ce82d21ea70c63
ddozza54 <bananamayong@gmail.com>
2024-09-18 01:18:09 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,4 @@
+function solution(age) {
+  const char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 's', 'y', 'z'];
+    return [...(age+'')].map(v=> char[v]).join('')
+}
