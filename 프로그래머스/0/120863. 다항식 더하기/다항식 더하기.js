df5d16dd6e646ad82a4f8c553d58299f78c433a0
ddozza54 <bananamayong@gmail.com>
2024-10-01 00:36:19 +0900

    [level 0] Title: 다항식 더하기, Time: 3.51 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,10 @@
+function solution(polynomial) {
+    let num = 0;
+    let xNum = 0;
+    polynomial.split(' + ').forEach(v=> {
+        Number(v) ? num += Number(v) : v==='x'? xNum += 1 : xNum += Number(v.replace('x', ''))});
+    console.log(num, xNum)
+    let x = xNum ? xNum == 1? 'x' : xNum+'x' : '';
+    let n = num ? num : '';
+    return x&&n ? `${x} + ${n}` :  !n ? `${x}` : `${n}` 
+}
