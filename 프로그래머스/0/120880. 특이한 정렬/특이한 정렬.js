dd680a5a6341c9b60fd228d2f8586093982c9416
ddozza54 <bananamayong@gmail.com>
2024-10-03 00:59:59 +0900

    [level 0] Title: 특이한 정렬, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,8 @@
+function solution(numlist, n) {
+  return numlist.sort((a, b)=>{
+      if(Math.abs(n-a) == Math.abs(n-b)){
+          return b-a
+      }
+      return Math.abs(n-a)-Math.abs(n-b)
+   })
+}
