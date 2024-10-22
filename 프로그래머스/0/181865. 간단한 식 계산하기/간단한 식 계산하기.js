22adf3cc08add94dd90e70ba312d3d32cf48270e
ddozza54 <bananamayong@gmail.com>
2024-10-22 16:45:26 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,6 @@
+function solution(binomial) {
+    const arr = binomial.split(' ');
+    if(arr[1] == '*') return Number(arr[0])*Number(arr[2])
+    if(arr[1] == '+') return Number(arr[0])+Number(arr[2])
+    if(arr[1] == '-') return Number(arr[0])-Number(arr[2])
+}
